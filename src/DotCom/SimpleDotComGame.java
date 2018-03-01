d8a775e176325e50939e738bb702707120f98d0a
万俟夜泱 <@192.168.1.102>
2018-03-01 16:47:30 +0800

    change

---
+++ src/DotCom/SimpleDotComGame.java
@@ -0,0 +1,5 @@
+package DotCom;
+
+public class SimpleDotComGame {
+
+}
--- src/DotCom/simpleDotComTestDrive.java
+++ src/DotCom/simpleDotComTestDrive.java
@@ -1,6 +1,6 @@
 package DotCom;
 
-public class simpleDotCom {
+public class simpleDotComTestDrive {
 	public static void main (String[] args){
 		SimpleDotCom dot=new SimpleDotCom();  //初始化一个simpleDotCom对象
 		
@@ -8,13 +8,6 @@ public class simpleDotCom {
 		dot.setLocationCells(locations); //调用dot com 的setter
 		
 		String userGuess="2";   //假的猜测
-		String result=dot.checkYourself(userGuess);   //调用被侧方法并传入假的数据
-		String testResult="failed";
-		if(result.equals("hit")){
-			testResult="passed";  //测试应该要返回 hit才算成功
-		}
-		System.out.println(testResult);
-		
-		
+		String result =dot.checkYourself(userGuess);   //调用被侧方法并传入假的数据
 	}
 }
